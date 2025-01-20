e7de8758013f3cbacc7f585343f27468087a9a71
NataGrigoreva <gr.natalij@gmail.com>
2025-01-20 16:23:00 +0300

    закрыли ключ

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import { Movies } from '../component/Movies';
 import { Preloader } from '../component/Preloader';
 import { Search } from '../component/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 
 class Main extends React.Component {
 
@@ -14,7 +16,7 @@ class Main extends React.Component {
     // Метод для выполнения поиска фильмов
     searthMovies = (str, type = 'all') => {
         this.setState({loading: true});
-        fetch(`http://www.omdbapi.com/?apikey=7450963b&s=${
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${
             str === '' ?
             'movie' 
             :
@@ -29,7 +31,7 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=7450963b&s=movie')
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=movie`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false}));   
     };
@@ -52,33 +54,4 @@ class Main extends React.Component {
 }
 
 export {Main}
-
-
-
-// import React, {useState, useEffect} from "react";
-// import { Preloader} from '../component/Preloader';
-// import { Movies } from '../component/Movies'
-
-// const API_URL = 'http://www.omdbapi.com/?apikey=7450963b&s=matrix';
-
-// export const Main = () => {
-//     const [movies, setMovies] = useState([]);
-//     const [loading, setLoading] = useState(true);
-
-//     useEffect(() => {
-//         fetch(API_URL)
-//         .then(response => response.json())
-//         .then(data => {
-//             setMovies(data.Search || []);
-//             setLoading(false)
-//         });
-//     }, []);
-
-//     return (
-//         <main className="container content">
-//             {loading ? <Preloader /> : <Movies movies={movies} />}
-//         </main>
-//     );
-// };
-
 
