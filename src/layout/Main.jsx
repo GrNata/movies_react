4705ee7fe4e979c05d5bcfedf55c5729611852a2
NataGrigoreva <gr.natalij@gmail.com>
2025-01-22 17:58:54 +0300

    рефакторинг Main

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Movies } from '../component/Movies';
 import { Preloader } from '../component/Preloader';
 import { Search } from '../component/Search';
@@ -6,17 +6,13 @@ import { Search } from '../component/Search';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 
-class Main extends React.Component {
+function Main() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [totalPage, setTotalPage] = useState(1);
 
-    state = {
-        movies: [],
-        loading: true,
-        totalPage: 1,
-    }
-
-    // Метод для выполнения поиска фильмов
-    searthMovies = (str, type = 'all', page) => {
-        this.setState({loading: true});
+    const searthMovies = (str, type = 'all', page) => {
+        setLoading(true);
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${
             str === '' ?
             'movie' 
@@ -32,37 +28,37 @@ class Main extends React.Component {
             const totalResult = (data.totalResults && !isNaN(parseInt(data.totalResults, 10)))
                      ? ( Math.ceil(parseInt(data.totalResults, 10) / 10) )
                      : 1;
-            this.setState({
-                movies: data.Search || [],      // Убедимся, что movies всегда массив
-                loading: false,
-                totalPage: totalResult,
 
+            setMovies(data.Search || []);      // Убедимся, что movies всегда массив
+            setLoading(false);
+            setTotalPage(totalResult);
+        
         });
-    });
-    }
+    };
 
-    componentDidMount() {
+    useEffect(() => {
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=movie`)
         .then(response => response.json())
         .then(data => {
             const totalPage = Math.ceil(parseInt(data.totalResults, 10) / 10);
-            this.setState({
-                movies: data.Search || [], 
-                loading: false,
-                totalPage,
-            });
-     });
 
-    };
+            setMovies(data.Search || []);
+            setLoading(false);
+        }).catch((err) => {
+            console.log(err);
+            setLoading(false);
+        }).catch((err) => {
+            console.log(err);
+            setLoading(false);
+        });
+    }, []);
 
-    render() {
-            const {movies, loading} = this.state;
 
             return <main className="conteiner content" >
                 {/* Передаём функцию searchMovies() в компонент Search через props */}
                         <Search  
-                            searthMovies={this.searthMovies } 
-                            totalPage={this.state.totalPage} > 0 ? {this.state.totalPage} : 1;
+                            searthMovies={searthMovies } 
+                            totalPage={totalPage} > 0 ? {totalPage} : 1;
                         </Search>
                         {
                             loading ? (
@@ -73,7 +69,7 @@ class Main extends React.Component {
                         }               
                     </main>
     }
-}
+
 
 export {Main}
 
